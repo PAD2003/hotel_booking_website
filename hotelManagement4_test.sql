ea45880c316cf00f0ea575b382585300453ddf05
quanhoang1408 <46843522+quanhoang1408@users.noreply.github.com>
2023-04-19 21:26:41 +0700

    new db

--- hotelManagement4_test.sql
+++ hotelManagement4_test.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Apr 11, 2023 at 04:56 PM
+-- Generation Time: Apr 17, 2023 at 10:34 AM
 -- Server version: 10.4.21-MariaDB
 -- PHP Version: 8.1.6
 
@@ -65,7 +65,8 @@ INSERT INTO `Customers` (`cusID`, `cusName`, `cusPhone`, `cusEmail`, `cusAddress
 (2, 'Customer 2', NULL, 'cus2@gmail.com', NULL, NULL, '2'),
 (3, 'Customer 3', '333333333', 'cus3@gmail.com', 'City 3', 'Male', '3'),
 (4, 'Customer 4', '444444444', 'cus4@gmail.com', 'City 4', 'Male', '4'),
-(8, 'Phan Anh Duc', '0932212003', 'phananhduc138@gmail.com', 'c', 'Male', 'anhduc2003');
+(8, 'Phan Anh Duc', '0932212003', 'phananhduc138@gmail.com', 'c', 'Male', 'anhduc2003'),
+(9, 'Hoàng Minh Quân', '0386981974', 'mq14082003@gmail.com', 'lo duc street', 'Male', '123');
 
 -- --------------------------------------------------------
 
@@ -107,16 +108,21 @@ CREATE TABLE `Hotels` (
   `hotelID` int(11) NOT NULL,
   `hotelName` varchar(100) NOT NULL,
   `location` varchar(200) NOT NULL,
-  `city_id` int(11) NOT NULL
+  `city_id` int(11) NOT NULL,
+  `link_map` varchar(255) DEFAULT NULL,
+  `link_img` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `Hotels`
 --
 
-INSERT INTO `Hotels` (`hotelID`, `hotelName`, `location`, `city_id`) VALUES
-(1, 'Hotel 1', 'City 1', 1),
-(2, 'Hotel 2', 'City 2', 1);
+INSERT INTO `Hotels` (`hotelID`, `hotelName`, `location`, `city_id`, `link_map`, `link_img`) VALUES
+(1, 'Hotel Commonwealth', 'Boston', 1, '!1m18!1m12!1m3!1d2948.7198761594327!2d-71.09781782335322!3d42.348495335831345!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89e37a1d2e734de5%3A0x59aae098bf93cb53!2sHotel%20Commonwealth!5e0!3m2!1svi!2s!4v1681228808627!5m2!1svi!2s\\', './hotel/1.jpeg'),
+(2, 'The Newbury', 'Newbury Street', 1, '!1m18!1m12!1m3!1d2948.5207857903442!2d-71.07409032396278!3d42.352739335564095!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89e37bb48cee3b0b%3A0x8df43a87561b77f5!2sThe%20Newbury%20Boston!5e0!3m2!1svi!2s!4v1681446862083!5m2!1svi!2s', './hotel/2.jpeg'),
+(5, 'Sofitel Legend Metropole', 'Hanoi', 2, '!1m18!1m12!1m3!1d3724.1816262139796!2d105.85380007528468!3d21.025417487893385!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3135abeb98f8b54d%3A0x90d6982234a65f25!2sSofitel%20Legend%20Metropole%20Hotel!5e0!3m2!1svi!2s!4v1681446674110!5m2!1svi!2s', './hotel/5.jpeg'),
+(6, 'XV Beacon', 'Beacon Street', 1, '!1m18!1m12!1m3!1d2948.2586364126173!2d-71.06450462396249!3d42.35832703521109!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89e3709b526f9701%3A0x3bc95bcb2380d36d!2sXV%20Beacon!5e0!3m2!1svi!2s!4v1681447169878!5m2!1svi!2s', './hotel/6.jpeg'),
+(7, 'PullMan Hanoi Hotel', '40 Cat Linh Str', 2, '!1m18!1m12!1m3!1d3724.0679649605413!2d105.82614427528472!3d21.029966387737055!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3135ab95bc23ffff%3A0x52a8917f5d1b7ef4!2sPullman%20Hanoi%20Hotel!5e0!3m2!1svi!2s!4v1681447236862!5m2!1svi!2s', './hotel/7.jpeg');
 
 -- --------------------------------------------------------
 
@@ -141,7 +147,9 @@ INSERT INTO `Reservations` (`resID`, `checkInDate`, `checkOutDate`, `reservedDat
 (14, '2022-12-01', '2022-12-02', '2022-11-30', 1, 8),
 (15, '2023-03-04', '2023-03-05', '2023-03-03', 3, 8),
 (16, '2023-04-10', '2023-04-13', '2023-04-07', 6, 8),
-(24, '2023-04-11', '2023-04-14', '2023-04-11', 6, 1);
+(24, '2023-04-11', '2023-04-14', '2023-04-11', 6, 1),
+(32, '2023-04-28', '2023-04-29', '2023-04-14', 1, 9),
+(33, '2023-04-16', '2023-04-17', '2023-04-14', 2, 9);
 
 -- --------------------------------------------------------
 
@@ -153,18 +161,20 @@ CREATE TABLE `Rooms` (
   `roomID` int(11) NOT NULL,
   `price` int(11) NOT NULL,
   `type` varchar(100) DEFAULT NULL,
-  `Hotels_hotelID` int(11) NOT NULL
+  `Hotels_hotelID` int(11) NOT NULL,
+  `capacity` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `Rooms`
 --
 
-INSERT INTO `Rooms` (`roomID`, `price`, `type`, `Hotels_hotelID`) VALUES
-(101, 100, 'Garden View', 1),
-(101, 100, 'Pool View', 2),
-(102, 100, 'Lake View', 1),
-(103, 150, 'Lake View', 1);
+INSERT INTO `Rooms` (`roomID`, `price`, `type`, `Hotels_hotelID`, `capacity`) VALUES
+(101, 100, 'Garden View', 1, 3),
+(101, 100, 'Pool View', 2, 5),
+(102, 100, 'Lake View', 1, 2),
+(103, 150, 'Lake View', 1, 2),
+(201, 200, 'City View', 7, 1);
 
 -- --------------------------------------------------------
 
@@ -186,6 +196,8 @@ INSERT INTO `Rooms_has_Reservations` (`Rooms_roomID`, `Rooms_Hotels_hotelID`, `R
 (101, 1, 14),
 (101, 1, 15),
 (101, 1, 24),
+(101, 1, 32),
+(101, 2, 33),
 (102, 1, 14),
 (102, 1, 15),
 (102, 1, 16),
@@ -262,7 +274,7 @@ ALTER TABLE `City`
 -- AUTO_INCREMENT for table `Customers`
 --
 ALTER TABLE `Customers`
-  MODIFY `cusID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `cusID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT for table `Employees`
@@ -274,13 +286,13 @@ ALTER TABLE `Employees`
 -- AUTO_INCREMENT for table `Hotels`
 --
 ALTER TABLE `Hotels`
-  MODIFY `hotelID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `hotelID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `Reservations`
 --
 ALTER TABLE `Reservations`
-  MODIFY `resID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
+  MODIFY `resID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=34;
 
 --
 -- Constraints for dumped tables
